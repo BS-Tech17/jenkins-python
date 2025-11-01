1b78d23d13ca49b25184b1c41a80e6dc173be3d7
Navya Gupta <guptanavya317@gmail.com>
2025-11-01 14:32:17 +0530

    flask app

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8081))
-    app.run(host="127.0.0.1", port=port, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=8081, debug=False, threaded=True)
